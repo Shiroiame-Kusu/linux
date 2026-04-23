3579586d6d5c05bb4e47b77a371c65c299c28273
楠ちゃん <kusu@kusu.moe>
2026-04-23 21:51:37 +0800

    Revert "Hopefully Fix kernel NULL pointer dereference"
    This reverts commit fd9611a638b9841336b804bff54e526b76906ebd.

--- fs/ntfs/aops.c
+++ fs/ntfs/aops.c
@@ -40,10 +40,6 @@ static void ntfs_iomap_read_end_io(struct bio *bio)
 static void ntfs_iomap_bio_submit_read(struct iomap_read_folio_ctx *ctx)
 {
 	struct bio *bio = ctx->read_ctx;
-
-	if (!bio)
-		return;
-
 	bio->bi_end_io = ntfs_iomap_read_end_io;
 	submit_bio(bio);
 }
